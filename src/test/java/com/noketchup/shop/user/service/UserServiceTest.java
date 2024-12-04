331e79ecbbce2167c38fdbfef337c3eaf77467cd
yrafe1998 <yousef000elmasry@gmail.com>
2024-12-04 14:15:20 +0300

    Unit tests and Some minor fixes

--- src/test/java/com/noketchup/shop/user/service/UserServiceTest.java
+++ src/test/java/com/noketchup/shop/user/service/UserServiceTest.java
@@ -1,10 +1,15 @@
-package com.noketchup.shop.user.controller;
+package com.noketchup.shop.user.service;
 
 import com.noketchup.shop.user.controller.dto.UserRequest;
+import com.noketchup.shop.user.controller.dto.UserResponse;
+import com.noketchup.shop.user.domain.UserDomainModel;
+import com.noketchup.shop.user.model.User;
 import com.noketchup.shop.user.repository.UserRepository;
-import com.noketchup.shop.user.service.UserServiceImpl;
+import com.noketchup.shop.user.service.mapper.UserMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -18,22 +23,63 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@Tag("Service")
 class UserServiceTest {
 
   @Mock
   UserRepository userRepository;
 
+  @Mock
+  UserMapper userMapper;
+
   @InjectMocks
   UserServiceImpl userService;
+
+  @Test
+  @DisplayName("Get User")
+  void testGetUser() {
+    UUID uuid = UUID.randomUUID();
+    when(userRepository.findById(any())).thenReturn(Optional.of(new User()));
+    when(userMapper.mapToUserResponse(any())).thenReturn(new UserResponse());
+    userService.getUser(uuid.toString());
+    verify(userRepository, times(1)).findById(any());
+
+    when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+    assertThrows(RuntimeException.class, () -> userService.getUser(uuid.toString()));
+  }
+
+  @Test
+  @DisplayName("Get User By Unique Parameters")
+  void testGetUserByUniqueParams() {
+    when(userRepository.findByEmailOrMobileNumberOrUsername(any(), any(), any())).thenReturn(Optional.of(new User()));
+    when(userMapper.mapToUserResponse(any())).thenReturn(new UserResponse());
+    UserResponse userResponse = userService.getUserByUniqueParam("", "", "");
+    assertNotNull(userResponse);
+
+    when(userRepository.findByEmailOrMobileNumberOrUsername(any(), any(), any())).thenReturn(Optional.empty());
+    assertThrows(RuntimeException.class, () -> userService.getUserByUniqueParam("", "", ""));
+  }
+
+  @Test
+  @DisplayName("Commit Domain")
+  void testCommitDomain(){
+    when(userRepository.save(any())).thenReturn(new User());
+    userService.commitDomain(new UserDomainModel());
+    verify(userRepository, times(1)).save(any());
+  }
 
   @DisplayName("Validate User Request")
   @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
